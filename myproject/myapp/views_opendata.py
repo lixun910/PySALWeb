2c8c596236f7449d7e451ab1a0e0ce2fe6fb155f
lixun910 <lixun910@gmail.com>
2014-12-13 12:40:06 -0700

    update layout opendata

--- myproject/myapp/views_opendata.py
+++ myproject/myapp/views_opendata.py
@@ -14,8 +14,8 @@ import logging, os, zipfile, shutil, time
 from hashlib import md5
 from views_utils import *
 from views_utils import _save_new_shapefile
+from Jobs import job_google_search
 import multiprocessing as mp
-import google
 
 logger = logging.getLogger(__name__)
 
@@ -48,39 +48,6 @@ def open_data(request):
             },
         context_instance=RequestContext(request)
     )
-
-def job_google_search(userid, uuid, q,bounds,apikey, name, opath,k=3):
-    start_time = time.time()
-    from myproject.myapp.models import Jobs
-    job = Jobs.objects.get(uuid=uuid)
-    job.status = 2
-    job.save()
-    from django.db import connection 
-    connection.close()
-    status = google.googlept(
-        bounds, q, apikey, lonx=False, bbox=True, k=k, ofile=opath
-    )
-    end_time = time.time()
-    try: 
-        job = Jobs.objects.get(uuid=uuid)
-        job.log = status
-        job.time = (end_time - start_time) / 60.0
-        if status == 'OK':
-            job.status = 3
-        job.save()
-        from django.db import connection 
-        connection.close()
-        
-        if status == 'OK':
-            _save_new_shapefile(userid, "GeoJSON", opath) 
-    except:
-        job = Jobs.objects.get(uuid=uuid)
-        job.log = status
-        job.time = (end_time - start_time) / 60.0
-        job.status = 4
-        job.save()
-        from django.db import connection 
-        connection.close()
 
 @login_required
 def google_search(request):
