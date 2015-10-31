9568dce81f2a5ae85a07bbbb4270bfc024e6f7b6
Xun Li <xunli@asu.edu>
2015-10-31 16:04:17 -0700

    add vizjson support for publishing

---
+++ media/js/ui/vizjson.js
@@ -0,0 +1,53 @@
+
+// Author: xunli at asu.edu
+define(['jquery'], function($) {
+
+var VizJsonReader = (function($){
+  var instance;
+  
+  function init() {
+    // singleton
+    
+    // private methods/vars
+    var id = "dlg-msg";
+    var titleID = "msg-title";
+    var contentID = "msg-content";
+   
+    var dlg = $('#' +id);
+    var titleEl = $('#' + titleID);
+    var contentEl = $('#' + contentID);
+    
+    dlg.dialog({
+      dialogClass: "dialogWithDropShadow",
+      width: 400, 
+      height: 300, 
+      autoOpen: false, 
+      modal: true,
+      buttons: {OK: function() {$( this ).dialog( "close" );}}
+    }).hide();
+    
+    return {
+      // public methods/vars
+      Show : function(title, content) {
+        titleEl.text(title);
+        contentEl.text(content);
+        dlg.dialog('open');
+      },
+      
+    };
+  };
+  
+  return {
+    getInstance : function() {
+      
+      if (!instance) {
+        instance = init();
+      }
+      
+      return instance;
+    },
+  };
+})($);
+
+return VizJsonReader;
+});
